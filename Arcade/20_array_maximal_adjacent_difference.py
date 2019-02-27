81e60f01faf7bc06ab1cf4bf27bc5fbc3537e6ec
Kevin Welch <kaedub16@gmail.com>
2019-02-26 23:31:00 -0800

    Solved arcade 20 array maximal adjacent difference

---
+++ Arcade/20_array_maximal_adjacent_difference.py
@@ -0,0 +1,12 @@
+# calculate the absolute value of the difference between each pair of items
+# 
+# set max to 0
+# for i in arr
+#    max = abs(arr[i] - arr[i-1]) if 
+
+def arrayMaximalAdjacentDifference(inputArray):
+    max = 0
+    for i in range(1,len(inputArray))
+        diff = abs(arr[i] - arr[i-1])
+        [max = diff if diff > max]
+        print(max)
